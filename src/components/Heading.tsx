8eb0700d77b1e54ec78c58de68abd5f70f5c2d65
jdpena28 <henrich@kynatech.ph>
2022-03-30 14:04:21 +0800

    add issues comment for responsiveness

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,6 +6,9 @@ const Heading = ({ headingTitle }: { headingTitle: string }) => {
       <h2 className="font-primary text-2xl tracking-wider text-primary">
         {headingTitle}
       </h2>
+      {/* green lines will be hidden on less than 1336px screen; 
+        on larger than 1920px the green lines will be not constraint on the left
+        NOTE: Find a work around */}
       <div className="absolute top-0 bottom-0 my-auto -ml-72 h-1 w-[14%] bg-emerald-500" />
     </div>
   );
